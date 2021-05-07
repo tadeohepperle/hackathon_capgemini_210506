4ebc6819900a80bd8051007e8af19f29b7275f3a
Tadeo <tadeo@do-mix.de>
2021-05-07 18:07:56 +0200

    build config changed

--- flutterproject/lib/main.dart
+++ flutterproject/lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: Constants.appTitle,
       theme: ThemeData(primarySwatch: Colors.green),
       home: OverarchingHomeComponent(),
